52e16ae30abfc78bb3f4f1cb6ec81ede5e327936
Pochang Lee <pochangl@usc.edu>
2018-03-03 10:40:23 +0800

    v1, predict action effect, train to stay away from end game

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class CartPole01:
     _states = [] # expected state
     states = [] # starting state
     actions = [] # actions
-    values = []
+    edges = []
     num_actions = env.action_space.n
     all_actions = list(range(num_actions))
 
@@ -18,7 +18,7 @@ class CartPole01:
     _state = tf.placeholder(tf.float32, shape=[None, 4], name='actual_states') # result state
     state = tf.placeholder(tf.float32, shape=[None, 4], name='state')
     action = tf.placeholder(tf.uint8, shape=[None], name='action')
-    _value = tf.placeholder(tf.float32, shape=[None], name='actual_value')
+    edge = tf.placeholder(tf.float32, shape=[None, 4], name='edges')
 
     """
         beginning of grpah
@@ -39,41 +39,47 @@ class CartPole01:
         one_hot_action = tf.one_hot(self.action, depth=self.num_actions)
 
         prediction_inputs = tf.concat([self.state, one_hot_action], 1)
-        self.prediction = prediction = self.fdnn(prediction_inputs, [6, 10, 4])
-        value = self.fdnn(prediction, [4, 1])
-        self.decision = tf.argmax(value)
+        self.prediction = prediction = self.fdnn(prediction_inputs, [6, 4, 4])
+        normalized_edge = tf.nn.l2_normalize(self.edge, axis=0)
+        print (normalized_edge)
+
+        self.value = tf.reduce_sum(tf.matmul(prediction, tf.transpose(normalized_edge)), axis=1)
+
+        self.decision = tf.argmin(self.value)
 
         sqrt = tf.squared_difference(self._state, prediction)
         self.prediction_cross_entropy = cross_entropy = tf.reduce_sum(sqrt)
         self.prediction_trainer = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-
-        sqrt = tf.squared_difference(value, self._value)
-        self.value_cross_entropy = cross_entropy = tf.reduce_sum(sqrt)
-        self.value_trainer = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-
 
     def train(self):
         train_dict = {
             self.state: self.states,
             self._state: self._states,
             self.action: self.actions,
-            self._value: self.values,
         }
         for _ in range(100):
-            _, _, value_loss, prediction_loss = self.session.run([self.value_trainer, self.prediction_trainer, self.value_cross_entropy, self.prediction_cross_entropy], feed_dict=train_dict)
-        print ('prediction: %10d value: %10d length: %5d' % (prediction_loss/len(self.values),  value_loss/len(self.values), len(self.values)), end='\t')
+            _, prediction_loss = self.session.run([self.prediction_trainer, self.prediction_cross_entropy], feed_dict=train_dict)
+        length = len(self.states)
+        print ('loss: %10d length: %5d' % (prediction_loss/length, length), end='\t')
         self.states = []
         self._states = []
         self.values = []
         self.actions = []
 
     def step(self, observation, step):
-        run_dict = {
-            self.state: np.tile(observation, (self.num_actions, 1)),
-            self.action: self.all_actions
-        }
-        ((decision,), ) = self.session.run([self.decision], feed_dict=run_dict)
-        observation = tuple(self.env.step(decision))
+        if not self.edges:
+            decision = self.env.action_space.sample()
+            observation = tuple(self.env.step(decision))
+        else:
+            states = np.tile(observation, (self.num_actions, 1))
+            run_dict = {
+                self.state: states,
+                self.action: self.all_actions,
+                self.edge: self.edges,
+            }
+            decision, value = self.session.run([self.decision, self.value], feed_dict=run_dict)
+            # print (value)
+            observation = tuple(self.env.step(decision))
         return decision, observation
 
 
@@ -84,7 +90,6 @@ class CartPole01:
             session.run(tf.global_variables_initializer())
             # first attempt
             for episode in range(50000):
-                values = []
                 observation = self.env.reset()
                 if not episode % 100:
                     total_steps = 0
@@ -93,18 +98,16 @@ class CartPole01:
                     action, (new_observation, reward, done, info) = self.step(observation, step)
                     self._states.append(new_observation)
                     self.states.append(observation)
-                    values.append(step)
                     self.actions.append(action)
                     observation = new_observation
 
                     if done:
+                        # print (observation)
+                        self.edges.append(observation)
                         total_steps = total_steps + step
-                        values = map(lambda v: v/step, values)
-                        self.values.extend(values)
-                        values = []
                         if episode % 100 == 99:
                             ones = list(filter(lambda v: v, self.actions))
-                            print ('action ratio: %f' % (len(ones)*1.0/ len(self.actions)), end=' ')
+                            print ('action ratio: %1.2f' % (len(ones)*1.0/ len(self.actions)), end=' ')
                             self.train()
                             print ('episode: %10d average step: %10f' % (episode, total_steps / 100))
                         break
